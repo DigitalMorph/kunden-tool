b0687efc9229d9a6f63a6db6621c4beeb45e7e39
Matthias <matthiasteich@gmx.net>
2025-05-31 18:48:33 +0200

    Fehler in kunden_db.py behoben

--- kunden_db.py
+++ kunden_db.py
@@ -16,7 +16,7 @@ authenticator = stauth.Authenticate(
     config["cookie"]["expiry_days"]
 )
 
-name, authentication_status, username = authenticator.login("Login", "main")
+name, authentication_status, username = authenticator.login("Login", location="main")
 
 if authentication_status is False:
     st.error("Benutzername oder Passwort ist falsch.")
